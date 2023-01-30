5d4e1b7817a4700458385f04335bc79c2b3cbdde
fefeRc <feliix.rocheteau@gmail.com>
2023-01-30 22:16:05 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+static string getMirrorString(string str)
+{
+    StringBuilder stringBldr = new StringBuilder();
+
+    var reversed = new string(str.Reverse().ToArray());
+
+    stringBldr.Append(reversed);
+
+    return stringBldr.ToString();
+}
+
+Console.WriteLine("Bonjour !");
+
+string inputUSer = Console.ReadLine();
+
+string reversedInputUser = getMirrorString(inputUSer);
+
+Console.WriteLine(reversedInputUser);
+
+if(inputUSer == reversedInputUser){Console.WriteLine("Bien dit !");}
+
+Console.WriteLine("Au revoir");
